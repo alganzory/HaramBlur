2b02ecf7a4a0a9ce5d0bc3c49eb66f96f34b9ddb
alganzory <md.alganzory@gmail.com>
2024-03-10 22:12:09 +0300

    refactor detector into class

--- src/modules/detector.js
+++ src/modules/detector.js
@@ -1,6 +1,7 @@
 // detector.js
 // This module exports detector functions and variables
 
+import * as Human from "../../tfjs/human.esm.js";
 const modelsUrl = chrome.runtime.getURL("src/assets/models/human");
 const nsfwUrl = chrome.runtime.getURL("src/assets/models/nsfwjs/model.json");
 
@@ -87,192 +88,218 @@ const getNsfwClasses = (factor = 0) => {
 	};
 };
 
-let human;
-let nsfwModel;
-let nsfwCache = {
-	predictions: [],
-	timestamp: 0,
-	skippedFrames: 0,
-	lastInputTensor: null,
-};
-
-const initHuman = async () => {
-	human = new Human.Human(HUMAN_CONFIG);
-	await human.load();
-	human.tf.enableProdMode();
-	// warmup the model
-	const tensor = human.tf.zeros([1, 224, 224, 3]);
-	await human.detect(tensor);
-	console.log("HB==Human model warmed up");
-	human.tf.dispose(tensor);
-};
-
-const humanModelClassify = async (tensor, needToResize) =>
-	new Promise((resolve, reject) => {
-		const promise = needToResize
-			? human.detect(tensor, {
-					filter: {
-						enabled: true,
-						width: needToResize?.newWidth,
-						height: needToResize?.newHeight,
-					},
-			  })
-			: human.detect(tensor);
-		promise
-			.then((res) => {
-				resolve(res);
-			})
-			.catch((err) => {
-				reject(err);
-			});
-	});
-
-const initNsfwModel = async () => {
-	// load the model from indexedDB if it exists, otherwise load from url
-	const indexedDBModel =
-		typeof indexedDB !== "undefined" && (await human.tf.io.listModels());
-
-	// if the model exists in indexedDB, load it from there
-	if (indexedDBModel?.["indexeddb://nsfw-model"]) {
-		nsfwModel = await human.tf.loadGraphModel("indexeddb://nsfw-model");
+class Detector {
+	constructor() {
+		console.log ("human", Human);
+		this._human = null;
+		this._nsfwModel = null;
+		this.nsfwCache = {
+			predictions: [],
+			timestamp: 0,
+			skippedFrames: 0,
+			lastInputTensor: null,
+		};
 	}
-	// otherwise load it from the url
-	else {
-		nsfwModel = await human.tf.loadGraphModel(nsfwUrl);
-		// save the model to indexedDB
-		await nsfwModel.save("indexeddb://nsfw-model");
+
+	get human() {
+		return this._human;
 	}
-	// console.log("HB==NSFW MODEL", nsfwModel);
-	const tensor = human.tf.zeros([1, 224, 224, 3]);
-	await nsfwModel.predict(tensor);
-	human.tf.dispose(tensor);
-	console.log("HB==NSFW model warmed up");
-};
 
-const nsfwModelSkip = async (input, config) => {
-	const tf = human.tf;
-	let skipFrame = false;
-	if (
-		config.cacheSensitivity === 0 ||
-		!input?.shape ||
-		input?.shape.length !== 4 ||
-		input?.shape[1] > 3840 ||
-		input?.shape[2] > 2160
-	)
-		return skipFrame; // cache disabled or input is invalid or too large for cache analysis
-
-	if (!nsfwCache.lastInputTensor) {
-		nsfwCache.lastInputTensor = tf.clone(input);
-	} else if (
-		nsfwCache.lastInputTensor.shape[1] !== input.shape[1] ||
-		nsfwCache.lastInputTensor.shape[2] !== input.shape[2]
-	) {
-		// input resolution changed
-		tf.dispose(nsfwCache.lastInputTensor);
-		nsfwCache.lastInputTensor = tf.clone(input);
-	} else {
-		const t = {};
-		t.diff = tf.sub(input, nsfwCache.lastInputTensor);
-		t.squared = tf.mul(t.diff, t.diff);
-		t.sum = tf.sum(t.squared);
-		const diffSum = await t.sum.data();
-		const diffRelative =
-			diffSum[0] /
-			(input.shape[1] || 1) /
-			(input.shape[2] || 1) /
-			255 /
-			3; // squared difference relative to input resolution and averaged per channel
-		tf.dispose([nsfwCache.lastInputTensor, t.diff, t.squared, t.sum]);
-		nsfwCache.lastInputTensor = tf.clone(input);
-		skipFrame = diffRelative <= (config.cacheSensitivity || 0);
+	get nsfwModel() {
+		return this._nsfwModel;
 	}
-	return skipFrame;
-};
+	
 
-const nsfwModelClassify = async (tensor, config = NSFW_CONFIG) => {
-	const tf = human.tf;
-	if (!tensor) return [];
-	let resized, expanded;
-	try {
-		const skipAllowed = await nsfwModelSkip(tensor, config);
-		const skipFrame = nsfwCache.skippedFrames < (config.skipFrames || 0);
-		const skipTime =
-			(config.skipTime || 0) >
-			(performance?.now?.() || Date.now()) - nsfwCache.timestamp;
+	initHuman = async () => {
+		this._human = new Human.Human(HUMAN_CONFIG);
+		await this._human.load();
+		this._human.tf.enableProdMode();
+		// warmup the model
+		const tensor = this._human.tf.zeros([1, 224, 224, 3]);
+		await this._human.detect(tensor);
+		this._human.tf.dispose(tensor);
+		console.log("HB==Human model warmed up");
+	};
 
-		// if skip is not allowed or skip time is not reached or skip frame is not reached or cache is empty then run the model
+	humanModelClassify = async (tensor, needToResize) => {
+		if (!this._human) await this.initHuman();
+		return new Promise((resolve, reject) => {
+			const promise = needToResize
+				? this._human.detect(tensor, {
+						filter: {
+							enabled: true,
+							width: needToResize?.newWidth,
+							height: needToResize?.newHeight,
+						},
+				  })
+				: this._human.detect(tensor);
+			promise
+				.then((res) => {
+					resolve(res);
+				})
+				.catch((err) => {
+					reject(err);
+				});
+		});
+	};
+
+	initNsfwModel = async () => {
+		// load the model from indexedDB if it exists, otherwise load from url
+		const indexedDBModel =
+			typeof indexedDB !== "undefined" &&
+			(await this._human.tf.io.listModels());
+
+		// if the model exists in indexedDB, load it from there
+		if (indexedDBModel?.["indexeddb://nsfw-model"]) {
+			this._nsfwModel = await this._human.tf.loadGraphModel("indexeddb://nsfw-model");
+		}
+		// otherwise load it from the url
+		else {
+			this._nsfwModel = await this._human.tf.loadGraphModel(nsfwUrl);
+			// save the model to indexedDB
+			await this._nsfwModel.save("indexeddb://nsfw-model");
+		}
+		// console.log("HB==NSFW MODEL", nsfwModel);
+		const tensor = this._human.tf.zeros([1, 224, 224, 3]);
+		await this._nsfwModel.predict(tensor);
+		this._human.tf.dispose(tensor);
+		console.log("HB==NSFW model warmed up");
+	};
+
+	nsfwModelSkip = async (input, config) => {
+		const tf = this._human.tf;
+		let skipFrame = false;
 		if (
-			!skipAllowed ||
-			!skipTime ||
-			!skipFrame ||
-			nsfwCache.predictions.length === 0
+			config.cacheSensitivity === 0 ||
+			!input?.shape ||
+			input?.shape.length !== 4 ||
+			input?.shape[1] > 3840 ||
+			input?.shape[2] > 2160
+		)
+			return skipFrame; // cache disabled or input is invalid or too large for cache analysis
+
+		if (!this.nsfwCache.lastInputTensor) {
+			this.nsfwCache.lastInputTensor = tf.clone(input);
+		} else if (
+			this.nsfwCache.lastInputTensor.shape[1] !== input.shape[1] ||
+			this.nsfwCache.lastInputTensor.shape[2] !== input.shape[2]
 		) {
-			// if size is not 224, resize the image
-			if (
-				tensor.shape[1] !== config.size ||
-				tensor.shape[2] !== config.size
-			) {
-				resized = tf.image.resizeNearestNeighbor(tensor, [
-					config.size,
-					config.size,
-				]);
-			}
-			// if 3d tensor, add a dimension
-			if (
-				(resized && resized.shape.length === 3) ||
-				tensor.shape.length === 3
-			) {
-				expanded = tf.expandDims(resized || tensor, 0);
-			}
-			const scalar = tf.scalar(config.tfScalar);
-			const normalized = tf.div(expanded || resized || tensor, scalar);
-			const logits = await nsfwModel.predict(normalized);
-
-			nsfwCache.predictions = await getTopKClasses(logits, config.topK);
-			nsfwCache.timestamp = performance?.now?.() || Date.now();
-			nsfwCache.skippedFrames = 0;
-
-			tf.dispose(
-				[scalar, normalized, logits]
-					.concat(expanded ? [expanded] : [])
-					.concat(resized ? [resized] : [])
-			);
+			// input resolution changed
+			tf.dispose(this.nsfwCache.lastInputTensor);
+			this.nsfwCache.lastInputTensor = tf.clone(input);
 		} else {
-			nsfwCache.skippedFrames++;
+			const t = {};
+			t.diff = tf.sub(input, this.nsfwCache.lastInputTensor);
+			t.squared = tf.mul(t.diff, t.diff);
+			t.sum = tf.sum(t.squared);
+			const diffSum = await t.sum.data();
+			const diffRelative =
+				diffSum[0] /
+				(input.shape[1] || 1) /
+				(input.shape[2] || 1) /
+				255 /
+				3; // squared difference relative to input resolution and averaged per channel
+			tf.dispose([this.nsfwCache.lastInputTensor, t.diff, t.squared, t.sum]);
+			this.nsfwCache.lastInputTensor = tf.clone(input);
+			skipFrame = diffRelative <= (config.cacheSensitivity || 0);
+		}
+		return skipFrame;
+	};
+
+	nsfwModelClassify = async (tensor, config = NSFW_CONFIG) => {
+		if (!this._human) await this.initHuman();
+		if (!this._nsfwModel) await this.initNsfwModel();	
+		const tf = this._human.tf;
+		if (!tensor) return [];
+		let resized, expanded;
+		try {
+			const skipAllowed = await this.nsfwModelSkip(tensor, config);
+			const skipFrame =
+				this.nsfwCache.skippedFrames < (config.skipFrames || 0);
+			const skipTime =
+				(config.skipTime || 0) >
+				(performance?.now?.() || Date.now()) - this.nsfwCache.timestamp;
+
+			// if skip is not allowed or skip time is not reached or skip frame is not reached or cache is empty then run the model
+			if (
+				!skipAllowed ||
+				!skipTime ||
+				!skipFrame ||
+				this.nsfwCache.predictions.length === 0
+			) {
+				// if size is not 224, resize the image
+				if (
+					tensor.shape[1] !== config.size ||
+					tensor.shape[2] !== config.size
+				) {
+					resized = tf.image.resizeNearestNeighbor(tensor, [
+						config.size,
+						config.size,
+					]);
+				}
+				// if 3d tensor, add a dimension
+				if (
+					(resized && resized.shape.length === 3) ||
+					tensor.shape.length === 3
+				) {
+					expanded = tf.expandDims(resized || tensor, 0);
+				}
+				const scalar = tf.scalar(config.tfScalar);
+				const normalized = tf.div(
+					expanded || resized || tensor,
+					scalar
+				);
+				const logits = await this._nsfwModel.predict(normalized);
+
+				this.nsfwCache.predictions = await this.getTopKClasses(
+					logits,
+					config.topK
+				);
+				this.nsfwCache.timestamp = performance?.now?.() || Date.now();
+				this.nsfwCache.skippedFrames = 0;
+
+				tf.dispose(
+					[scalar, normalized, logits]
+						.concat(expanded ? [expanded] : [])
+						.concat(resized ? [resized] : [])
+				);
+			} else {
+				this.nsfwCache.skippedFrames++;
+			}
+
+			return this.nsfwCache.predictions;
+		} catch (error) {
+			console.error("HB==NSFW Detection Error", resized || tensor, error);
+		}
+	};
+
+	getTopKClasses = async (logits, topK) => {
+		const values = await logits.data();
+
+		const valuesAndIndices = [];
+		for (let i = 0; i < values.length; i++) {
+			valuesAndIndices.push({ value: values[i], index: i });
+		}
+		valuesAndIndices.sort((a, b) => {
+			return b.value - a.value;
+		});
+		const topkValues = new Float32Array(topK);
+		const topkIndices = new Int32Array(topK);
+		for (let i = 0; i < topK; i++) {
+			topkValues[i] = valuesAndIndices[i].value;
+			topkIndices[i] = valuesAndIndices[i].index;
 		}
 
-		return nsfwCache.predictions;
-	} catch (error) {
-		console.error("HB==NSFW Detection Error", resized || tensor, error);
-	}
-};
-
-async function getTopKClasses(logits, topK) {
-	const values = await logits.data();
-
-	const valuesAndIndices = [];
-	for (let i = 0; i < values.length; i++) {
-		valuesAndIndices.push({ value: values[i], index: i });
-	}
-	valuesAndIndices.sort((a, b) => {
-		return b.value - a.value;
-	});
-	const topkValues = new Float32Array(topK);
-	const topkIndices = new Int32Array(topK);
-	for (let i = 0; i < topK; i++) {
-		topkValues[i] = valuesAndIndices[i].value;
-		topkIndices[i] = valuesAndIndices[i].index;
-	}
-
-	const topClassesAndProbs = [];
-	for (let i = 0; i < topkIndices.length; i++) {
-		topClassesAndProbs.push({
-			className: getNsfwClasses()?.[topkIndices[i]].className,
-			probability: topkValues[i],
-			id: topkIndices[i],
-		});
-	}
-	return topClassesAndProbs;
+		const topClassesAndProbs = [];
+		for (let i = 0; i < topkIndices.length; i++) {
+			topClassesAndProbs.push({
+				className: getNsfwClasses()?.[topkIndices[i]].className,
+				probability: topkValues[i],
+				id: topkIndices[i],
+			});
+		}
+		return topClassesAndProbs;
+	};
 }
 
 const containsNsfw = (nsfwDetections, strictness) => {
@@ -319,11 +346,7 @@ const containsGenderFace = (detections, detectMale, detectFemale) => {
 	}
 
 	const faces = detections.face;
-	if (detectMale && detectFemale)
-		return faces.some(
-			(face) =>
-				face.age > 20 
-		);
+	if (detectMale && detectFemale) return faces.some((face) => face.age > 20);
 	if (detectMale || detectFemale)
 		return faces.some(
 			(face) =>
@@ -338,15 +361,4 @@ const containsGenderFace = (detections, detectMale, detectFemale) => {
 	else return false;
 };
 // export the human variable and the HUMAN_CONFIG object
-export {
-	initHuman,
-	initNsfwModel,
-	nsfwModel,
-	human,
-	HUMAN_CONFIG,
-	nsfwModelClassify,
-	humanModelClassify,
-	getNsfwClasses,
-	containsNsfw,
-	containsGenderFace,
-};
+export { getNsfwClasses, containsNsfw, containsGenderFace, Detector};
